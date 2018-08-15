069acbfab270f49919b3806d6e36d57b0298e3ef
Alexander Lumphé <lumphe@Alexanders-Mini.kasenet>
2018-08-15 18:30:32 +0200

    added command line support

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import resolve from 'rollup-plugin-node-resolve';
 import uglify from 'rollup-plugin-uglify';
 import commonjs from 'rollup-plugin-commonjs';
 
-export default {
+export default [{
     experimentalCodeSplitting: true,
     experimentalDynamicImport: true,
     input: [
@@ -34,4 +34,39 @@ export default {
         uglify(),
         commonjs()
     ]
-};
+},{
+    experimentalCodeSplitting: true,
+    experimentalDynamicImport: true,
+    input: [
+        "esm/bin.js",
+    ],
+    output: [
+        {
+            banner: "#!/usr/bin/env node",
+            dir: "dist",
+            format: "cjs",
+            name: "node-ftp",
+            exports: "named",
+            sourcemap: true
+        }
+    ],
+    external: [
+        "fs",
+
+        "tls",
+        "zlib",
+        "net",
+        "events",
+        "util",
+        "stream",
+        "string_decoder",
+        "readline",
+        "url",
+    ],
+    plugins: [
+        resolve(),
+        uglify(),
+        commonjs()
+    ]
+}
+];
---
+++ src/bin.ts
@@ -0,0 +1,134 @@
+import { URL } from "url";
+import Client from "./connection";
+
+import { createInterface } from "readline";
+
+const args = process.argv.slice(2);
+
+const url = new URL(args[0]);
+const opt = {
+    host: url.host,
+    password: url.password,
+    port: url.port ? parseInt(url.port, 10) : undefined,
+    secure: url.protocol === "ftps",
+    user: url.username,
+};
+// TODO: read user and pass if none exists
+const knownCmd = ["list", "get", "put"];
+Client.connect(opt).then((c) => {
+    const completer = (line: string, cb: (e: Error | null, r: [string[], string]) => void) => {
+        const hits = knownCmd.filter((cmd) => cmd.startsWith(line));
+        cb(null, [hits.length ? hits : knownCmd, line]);
+    };
+
+    const r1 = createInterface({
+        completer,
+        input: process.stdin,
+        output: process.stdout,
+        prompt: "ftps-ts> ",
+    });
+    return new Promise((res) => {
+        r1.on("SIGINT", async () => {
+            try {
+                await c.logout();
+            } catch (e) {
+                r1.write(e);
+            }
+            process.stdin.pause();
+            res();
+        });
+        r1.on("line", async (line) => {
+            const params = line.split(" ");
+            // TODO: repare escpade spaces in string
+            const cmd = params[0];
+            if (cmd === "list") {
+                try {
+                    console.dir(await c.list());
+                } catch (e) {
+                    r1.write(e);
+                }
+            } else if (cmd === "get") {
+                const path = params[1];
+                if (path !== undefined) {
+                    const compression = params[2] ? params[2] === "true" : undefined;
+                    try {
+                        await c.get(path, compression);
+                    } catch (e) {
+                        r1.write(e);
+                    }
+                } else {
+                    r1.write("Path are need to do get.");
+                }
+            } else if (cmd === "put") {
+                const path = params[1];
+                const dest = params[2];
+                if (path !== undefined && dest !== undefined) {
+                    const compression = params[2] ? params[2] === "true" : undefined;
+                    try {
+                        await c.put(path, dest, compression);
+                    } catch (e) {
+                        r1.write(e);
+                    }
+                } else {
+                    r1.write("Path to file and destination are needed.");
+                }
+            } else if (cmd === "append") {
+                const path = params[1];
+                const dest = params[2];
+                if (path !== undefined && dest !== undefined) {
+                    const compression = params[2] ? params[2] === "true" : undefined;
+                    try {
+                        await c.append(path, dest, compression);
+                    } catch (e) {
+                        r1.write(e);
+                    }
+                } else {
+                    r1.write("Path to file and destination are needed.");
+                }
+            } else if (cmd === "rename") {
+                const oldPath = params[1];
+                const newPath = params[2];
+                if (oldPath !== undefined && newPath !== undefined) {
+                    try {
+                        await c.rename(oldPath, newPath);
+                    } catch (e) {
+                        r1.write(e);
+                    }
+                } else {
+                    r1.write("Old and new path are needed.");
+                }
+            } else if (cmd === "logout" || cmd === "exit") {
+                try {
+                    await c.logout();
+                } catch (e) {
+                    r1.write(e);
+                }
+                process.stdin.pause();
+                res();
+            } else if (cmd === "delete") {
+                const path = params[1];
+                if (path) {
+                    try {
+                        await c.delete(path);
+                    } catch (e) {
+                        r1.write(e);
+                    }
+                } else {
+                    r1.write("Path needed to delete a file.");
+                }
+            } else if (cmd === "cwd") {
+                // TODO: Do
+            } else if (cmd === "abort") {
+                // TODO: Do
+            } else if (cmd === "site") {
+                // TODO: Do
+            } else if (cmd === "status") {
+                // TODO: Do
+            } else if (cmd === "ascii") {
+                // TODO: Do
+            } else if (cmd === "binary") {
+                // TODO: Do
+            }
+        });
+    });
+});
